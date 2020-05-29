38e55eb48ee33a37b01f5da56581f5745255353c
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-29 11:12:46 -0500

    esther

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,17 @@
 class Vendor < ApplicationRecord
+    has_many :vendor_sweets
+    has_many :sweets, through: :vendor_sweets
 
+    def avg_price
+        sum = 0
+        if self.vendor_sweets.length != 0
+            self.vendor_sweets.each do |vs|
+                sum += vs.price
+            end
+            result = sum / self.vendor_sweets.length
+        else
+            result = 0
+        end
+        result
+    end
 end
