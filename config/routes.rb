12d2f82c08902f7c27905636f51fcc8258e1ec66
github username <github email address>
2020-05-29 12:06:36 -0400

    thach

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sweets', to: "sweets#index", as: "sweets"
-  get '/vendors', to: "vendors#index", as: "vendors"
+  get 'vendor_sweets/new'
 
+  get 'vendor_sweets/create'
+
+  get 'vendor_sweets/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :vendors, only: [:index, :show]
+  resources :vendor_sweets, only: [:index, :show, :create, :new, :destroy]
+  resources :sweets, only: [:index, :show]
 end
---
+++ vendor/bundle/gems/ffi-1.9.18/ext/ffi_c/libffi-x86_64/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/thachluu/Development/code/module-2-code-challenge-round-2-hou01-seng-f-module-2-code-challenge---vendors-and-sweets-1590762762/vendor/bundle/gems/ffi-1.9.18/ext/ffi_c/libffi/src/x86/ffitarget.h
